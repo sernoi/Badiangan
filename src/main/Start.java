400127a0dd2b6164391a4bd83a9396ae21132598
John Rey Alipe <John Rey Alipe@SernoiLaptop>
2019-09-24 16:37:00 +0800

    new commit

---
+++ src/main/Start.java
@@ -0,0 +1,11 @@
+package main;
+
+import login.LoginFrame;
+public class Start 
+{
+    public static void main(String[] args)
+    {
+        LoginFrame lf = new LoginFrame();
+        lf.setVisible(true);
+    }
+}
