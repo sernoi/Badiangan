01d18d2fff476d7e8a4c180b52e204a07152443b
John Rey Alipe <John Rey Alipe@SernoiLaptop>
2020-03-14 09:16:17 +0800

    added settings and access to file remotely

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -14,16 +14,17 @@ import org.jxmapviewer.viewer.TileFactoryInfo;
 public class Settings 
 {
     //user of the database
-    public static String userName = "troj";  
+    public static String userName = "root";  
     //password
     public static String password = "1234";
     //url of db
-    public static String url = "jdbc:mysql://192.168.1.250/badiangan";
+    //public static String url = "jdbc:mysql://192.168.1.250/badiangan";
+    public static String url = "jdbc:mysql://localhost/badiangan";
     
     //offline map
-    //public static TileFactoryInfo info = new OSMTileFactoryInfo("","jar:file:tiles/tiles.zip!");
+    public static TileFactoryInfo info = new OSMTileFactoryInfo("","jar:file:tiles/tiles.zip!");
     //online map
-    public static TileFactoryInfo info = new OSMTileFactoryInfo();
+    //public static TileFactoryInfo info = new OSMTileFactoryInfo();
     
     //string url inputstreamo of report
     public static String in = "http://192.168.1.250:8080/badiangan/report.pdf";
